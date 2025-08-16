a06a2c984bceb75c2cd72b6784d548e975ad7a72
narab0721 <narab2107@gmail.com>
2025-08-16 13:56:33 +0530

    Build configuration

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/vibha-stock-alerts/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
+})
