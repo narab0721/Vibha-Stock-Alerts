39e19eb80f6cf3d87215c301ea2d634785f1612c
Narasimharao Balusupati <narab2107@gmail.com>
2025-08-17 22:39:20 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        'marquee': 'marquee 45s linear infinite',
-        'marquee-mobile': 'marquee 30s linear infinite',
-      },
-      keyframes: {
-        marquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-33.333%)' },
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
