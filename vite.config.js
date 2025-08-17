47c824792a34a64acbc40f727ff1c1c9a391109e
Narasimharao Balusupati <narab2107@gmail.com>
2025-08-17 22:39:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/vibha-stock-alerts/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false
+  server: {
+    host: true,
+    port: 5173
   }
 })
